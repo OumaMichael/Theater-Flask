ba5b7137b249aee79fd3c2f74f917a4e3d1d3bfe
OumaMichael <oumamichael108@gail.com>
2025-08-30 13:06:27 +0300

    Update main.js

--- theater/static/js/main.js
+++ theater/static/js/main.js
@@ -5,12 +5,6 @@ var interactiveForm = function(callback = null){
     var numSeats = $('#seats option:selected').val();
     if (numSeats === undefined)
         numSeats = 1;
-    
-    if (!CProj) {
-        console.error("No projection selected");
-        return;
-    }
-    
     $.ajax({
         type: "POST",
         url: "/ajax",
@@ -21,39 +15,35 @@ var interactiveForm = function(callback = null){
                 callback(result)
             }
             else{
-                if (result && result.result && result.result[CProj] !== undefined) {
-                    if (result.result[CProj] > 0)
-                    {
-                        $("#more-seats").show();
-                        $("#no-more-seats").hide();
-                        $("#seats").find('option').not(':first').remove();
-                        for (var i = 1; i <= result.result[CProj]; i++) {
-                            var stri = i.toString();
-                            if (i != 1){
-                                $("#seats").append($('<option>', {value:stri, text:stri}));
-                            }
+                // console.log(result.result[CProj]);
+                // console.log(result.result);
+
+                if (result.result[CProj] > 0)
+                {
+                    $("#more-seats").show();
+                    $("#no-more-seats").hide();
+                    $("#seats").find('option').not(':first').remove();
+                    for (var i = 1; i <= result.result[CProj]; i++) {
+                        var stri = i.toString();
+                        if (i != 1){
+                            $("#seats").append($('<option>', {value:stri, text:stri}));
                         }
-                        var price = 5;  // think if use price as attribute or fixed
-                        var totalPrice = price * numSeats;
-                        var strprice = 'Price:&nbsp;&nbsp;('+ price.toString()+ 'x'+ numSeats +') = '+totalPrice.toString() + '€';
-                        $("#price").empty();
-                        $("#price").append(strprice);
-                    }else{
-                        $("#more-seats").hide();
-                        $("#no-more-seats").show();
                     }
-                } else {
-                    console.error("Invalid response format from server");
+                    var price = 5;  // think if use price as attribute or fixed
+                    var totalPrice = price * numSeats;
+                    var strprice = 'Price:&nbsp;&nbsp;('+ price.toString()+ 'x'+ numSeats +') = '+totalPrice.toString() + '€';
+                    $("#price").empty();
+                    $("#price").append(strprice);
+                }else{
+                    $("#more-seats").hide();
+                    $("#no-more-seats").show();
                 }
+
             }
             
         },
         error: function(error){
-            console.error("AJAX error:", error);
-            // Show error message to user
-            $("#more-seats").hide();
-            $("#no-more-seats").show();
-            $("#no-more-seats").html("<p style='color:red;'>Error loading seat availability. Please refresh the page.</p>");
+            console.log(error);
         },
     });
 
@@ -64,12 +54,6 @@ var Price = function(){
     var numSeats = $('#seats option:selected').val();
     if (numSeats === undefined)
         numSeats = 1;
-    
-    if (!CProj) {
-        console.error("No projection selected for price calculation");
-        return;
-    }
-    
     var price = 5;  // think if use price as attribute or fixed
     var totalPrice = price * numSeats;
     var strprice = 'Price:&nbsp;&nbsp;('+ price.toString()+ 'x'+ numSeats +') = '+totalPrice.toString() + '€';
@@ -80,19 +64,15 @@ var Price = function(){
 $(document).ready(function(){
     
     interactiveForm (function(data) {
-        if (data && data.result) {
-            for (var key in data.result){
-                if (data.result[key] <= 0){
-                    $("#proj" + key.toString() + " td.seats").html("<p style='color:red;'>0<p>");
-                    $("#proj" + key.toString() + " td.sold").html("<a class='red'>SOLD OUT</a>")
-                }else{
-                    $("#proj" + key.toString() + " td.seats").html(data.result[key]);
-                }
+        // console.log(data['result']);
+
+        for (var key in data['result']){
+            if (data['result'][key] <= 0){
+                $("#proj" + key.toString() + " td.seats").html("<p style='color:red;'>0<p>");
+                $("#proj" + key.toString() + " td.sold").html("<a class='red'}}'>SOLD OUT</a>")
+            }else{
+                $("#proj" + key.toString() + " td.seats").html(data['result'][key]);
             }
-        } else {
-            console.error("Invalid data received for seat availability");
-            // Show loading error in all seat cells
-            $("td.seats").html("<span style='color:red;'>Error</span>");
         }
     });
     
